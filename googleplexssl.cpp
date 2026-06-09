7b754988efdd42b481b4bbff22a92a6859cbf5df
fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>
2026-06-09 19:04:19 +0700

    Create googleplexssl.cpp
    Signed-off-by: fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>

---
+++ googleplexssl.cpp
@@ -0,0 +1,20 @@
+# target blokded cheat and gambling game 
+# Retrieved 2026-04-14, License - CC BY-SA 4.0
+# cyberw 1 r y 4 development tester and target to ssl and serveral 
+requests.exceptions.SSL: HTTPSConnectionPool(host=':https://www.google.co.id/android' 
+port=443): Max retries exceeded with url: / (Caused by SSL("Can 
+connect to HTTPS URL because the SSL module is available."))
+
+# target blokded cheat and gambling game 
+# Retrieved 2026-04-14, License - CC BY-SA 4.0
+# cyberw 1 r y 4 development tester and target to ssl and serveral 
+requests.exceptions.SSL: HTTPSConnectionPool(host=':https://www.google.co.id' 
+port=103): Max retries exceeded with url: / (Caused by SSL("Can 
+connect to HTTPS URL because the SSL module is available."))
+
+# target blokded cheat and gambling game 
+# Retrieved 2026-04-14, License - CC BY-SA 4.0
+# cyberw 1 r y 4 development tester and target to ssl and serveral 
+requests.exceptions.SSL: HTTPSConnectionPool(host=':https://www.google.co.id/' 
+port=172): Max retries exceeded with url: / (Caused by SSL("Can 
+connect to HTTPS URL because the SSL module is available."))
